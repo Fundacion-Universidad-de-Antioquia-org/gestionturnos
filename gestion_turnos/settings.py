f770f15bd21f18f0c51afed3a915928ab31c3c26
srivera227 <desarrolladorti3@fundacionudea.org.co>
2025-08-20 10:54:40 -0500

    20/08/2025
    20/08/2025

--- gestion_turnos/settings.py
+++ gestion_turnos/settings.py
@@ -85,15 +85,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestion_turnos.wsgi.application'
 LOGIN_URL = "/login"
-LOGIN_REDIRECT_URL = "/home/"    # Or any other endpoint
+LOGIN_REDIRECT_URL = "account/home/"    # Or any other endpoint
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
