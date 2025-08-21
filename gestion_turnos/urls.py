784a9aafa12e98cf6824793ee0dd83d462d89c50
srivera227 <desarrolladorti3@fundacionudea.org.co>
2025-08-21 16:19:13 -0500

    Corrección
    Cuando se accede a la raíz del proyecto lo redirijo al login.

--- gestion_turnos/urls.py
+++ gestion_turnos/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     settings.AUTH.urlpattern,
     path('admin/', admin.site.urls),
     path('account/',include('asignacion_turnos.urls')),
+    path('', TemplateView.as_view(template_name='account/home.html'), name='home'),
 ]
