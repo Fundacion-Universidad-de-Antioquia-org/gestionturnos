4fd469da082355dee8653394ee3dbf16a18dddaa
srivera227 <desarrolladorti3@fundacionudea.org.co>
2025-10-14 08:16:55 -0500

    -- 14/10/2025

--- gestion_turnos/settings.py
+++ gestion_turnos/settings.py
@@ -115,16 +115,16 @@ DATABASES = {
     }
 }
 
-#DATABASES = {
-   #'default': {
-       # 'ENGINE': 'django.db.backends.postgresql',
-      #  'NAME': os.getenv('DB_NAME'),
-     #   'USER': os.getenv('DB_USER'),
-    #    'PASSWORD': os.getenv('DB_PASSWORD'),
-   #     'HOST': os.getenv('DB_HOST'),
-  #      'PORT': '5432',
- #   }
-#}
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+       'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
+   }
+}
 
 
 # Password validation
