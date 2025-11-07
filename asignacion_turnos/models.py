27033d99b335cc05726a70e76daa4cbf22ce6064
srivera227 <desarrolladorti3@fundacionudea.org.co>
2025-11-07 12:02:41 -0500

    Correccion

--- asignacion_turnos/models.py
+++ asignacion_turnos/models.py
@@ -120,7 +120,7 @@ class Cambios_de_turnos(models.Model):
     zonaReceptor = models.CharField(max_length=20, null=True)
     transportable_solicitante = models.BooleanField(default=False)
     transportable_receptor = models.BooleanField(default=False)
-    horaAprobacion =models.DateTimeField()
+    horaAprobacion = models.DateTimeField(null=True)
     
 class Solicitudes_Gt(models.Model):
     cargo = models.CharField(max_length=100)
